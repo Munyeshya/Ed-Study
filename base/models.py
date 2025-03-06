4a3e59fda6c9608e3dd11cc806c9abc65e7aca49
Munyeshya <obedbenny54@gmail.com>
2025-03-06 20:53:21 +0200

    Create admin & notofication

--- base/models.py
+++ base/models.py
@@ -42,3 +42,20 @@ class Payment(models.Model):
 
     def __str__(self):
         return f"Payment of {self.amount} by {self.student.full_name}"
+
+class Notification(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='notifications')
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.student.full_name}"
+ 
+ from django.contrib.auth.models import AbstractUser
+
+class AdminUser(AbstractUser):
+    role = models.CharField(max_length=50, default='admin')
+
+    def __str__(self):
+        return self.username
